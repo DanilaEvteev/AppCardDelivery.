188b5ce6cb23467950fa2db62153feb83ff39b79
DanilaEvteev <EvteevDanilaAndreevich@yandex.ru>
2023-07-30 22:52:32 +0300

    Test_2

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,38 +1,37 @@
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.*;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 
 public class AppCardDeliveryTest {
-    LocalDate today = LocalDate.now();
-    LocalDate newDate = today.plusDays(3);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    @BeforeEach
-    void setUp() {
-        open("http://localhost:7777");
+
+    private String date(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
+
     @Test
-    public void shouldFullAllFormCorrect() {
-
-        $("[data-test-id=city] input").setValue("Казань");
-        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
+    void successfulAuthorization() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:7777/");
+        $x("//input[contains(@placeholder,'Город')]").setValue("Казань");
+        String currentDate = date(4, "dd.MM.yyyy");
+        $x("//input[contains(@placeholder,'Дата встречи')]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $x("//input[contains(@placeholder,'Дата встречи')]").setValue(currentDate);
         $("[data-test-id=name] input").setValue("Тестовый Тест");
-        $("[data-test-id=phone] input").setValue("+79211115522");
+        $("[data-test-id=phone] input").setValue("+79215552211");
         $("[data-test-id=agreement]").click();
-        $(".button").click();
-        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + newDate.format(formatter)));
-
+        $x("//span[contains(text(),'Забронировать')]").click();
+        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(12));
+        $x("//div[contains(@class, 'notification__content')]").shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
     }
 }
